40d55a36e31f91310a0379db2d9cfa372b7f6dbf
Jason Livesay <ithkuil@gmail.com>
2015-06-05 20:09:10 +0000

    mod name

--- src/bin/index.js
+++ src/bin/index.js
@@ -18,11 +18,11 @@ program
 .command('newapp <service>')
 .description('add a new app and get a token/key for a service')
 .action( (service) => {
-  var script = require.resolve(service);
-  var lib = path.dirname(script);
-  var fname = lib+'/signupbot.js';
-  exec('cd '+__dirname+'/../../node_modules/musepm-signupbot;' +
-       'npm run signup ' + fname);
+  let script = require.resolve(`musepm-${service}`);
+  let lib = path.dirname(script);
+  let fname = `${lib}/signupbot.js`;
+  exec(`cd ${__dirname}/../../node_modules/musepm-signupbot;` +
+       `npm run signup ${fname}`);
 });
 
 program.parse(process.argv);
