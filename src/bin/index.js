4cda838543dd39eccf7370191c5a461a699054ab
Jason Livesay <ithkuil@gmail.com>
2015-06-05 19:39:58 +0000

    relative path

--- src/bin/index.js
+++ src/bin/index.js
@@ -18,8 +18,11 @@ program
 .command('newapp <service>')
 .description('add a new app and get a token/key for a service')
 .action( (service) => {
+  var script = require.resolve(service);
+  var lib = path.dirname(script);
+  var fname = lib+'/signupbot.js';
   exec('cd '+__dirname+'/../../node_modules/musepm-signupbot;' +
-       'npm run signup ' + service);
+       'npm run signup ' + fname);
 });
 
 program.parse(process.argv);
